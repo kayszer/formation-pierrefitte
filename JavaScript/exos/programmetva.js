a873ffbbfadb40647c4960d862861ad2dfe159ff
kayszer <kassas.kayes93@gmail.com>
2019-01-14 16:48:49 +0100

    jurassic

--- JavaScript/exos/programmetva.js
+++ JavaScript/exos/programmetva.js
@@ -1,5 +1,15 @@
-var tva = 1.80
-var chiffre = prompt("saisir un motant");
+// var tva = 1.20
+// var chiffre = prompt("saisir un motant");
 
+// document.write("le montant HTTC de votre article est de " + " " + chiffre*tva);
 
-document.write("le montant HTTC de votre article est de " + " " + chiffre*tva);
+correction:
+var prixHT = Number(prompt("Saisir un prix hors taxes"));
+
+// tva = prixHT / 100 * 20;
+
+// prixTTC = tva + prixHT;
+
+// alert(prixTTC);
+// console.log(prixTTC); /** Afficher dans la console */
+// document.write(prixTTC); /** Afficher dans la page */
