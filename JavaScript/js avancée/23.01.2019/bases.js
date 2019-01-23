e45b1bdb42b3cd01f64cbd2a7cb029929e102e7a
kayszer <kassas.kayes93@gmail.com>
2019-01-23 17:27:40 +0100

    js wf3

---
+++ JavaScript/js avancée/23.01.2019/bases.js
@@ -0,0 +1,3 @@
+// document.getElementById("txt").innerText = "salut tous le monde"
+
+console.log('javascript est en marche')
---
+++ JavaScript/js avancée/23.01.2019/var.js
@@ -0,0 +1,28 @@
+// var i = 0; //Number
+// var i = "0"; //String
+// var done = false; // Boolean
+// i = []; // Tableau
+// i = {}; // Objet
+
+
+// var tabPrenoms = ["Rosita","Bob","Gaston"]; // Tableau
+// var tabNoms = []; //Tableau
+
+// var nom = 'gaston'
+// var prenom = 'bob'
+
+// var nomComplet = prenom +" "+ nom
+// var i=3
+// nomComplet = prenom+i
+
+// var d = new Date ();
+// var y = d.getFullYear();
+// var m = d.getMonth();
+// var dStr = d.toLocaleDateString();
+// var heure = d.toLocaleTimeString();
+
+var prenom = ["kayszer","Sozer","Kobayashi"];
+
+for(var i=0; i<prenom.length; i++){
+    window.alert("le prenom est"+" "+prenom[i])
+}
